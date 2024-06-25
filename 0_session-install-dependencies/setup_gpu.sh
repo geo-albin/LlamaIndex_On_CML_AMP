aaf9aa3b017fb2d80bc2037568d13eeecab468f0
Albin George <ageorge@cloudera.com>
2024-06-24 21:09:26 -0400

    fix numpy issues

--- 0_session-install-dependencies/setup_gpu.sh
+++ 0_session-install-dependencies/setup_gpu.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 echo "installing GPU version of llama-cpp"
-pip install llama-cpp-python numpy==1.26.4 --upgrade --force-reinstall --no-cache-dir --extra-index-url https://abetlen.github.io/llama-cpp-python/whl/cu122
+
+pip install llama-cpp-python==0.2.68 --extra-index-url https://abetlen.github.io/llama-cpp-python/whl/cu122
