c58ec196865700e2b0abdba3c35b337197d576b8
Albin George <ageorge@cloudera.com>
2024-06-24 22:01:26 -0400

    GPU

--- 0_session-install-dependencies/setup_gpu.sh
+++ 0_session-install-dependencies/setup_gpu.sh
@@ -7,5 +7,5 @@ echo "installing GPU version of llama-cpp"
 # pip install llama-cpp-python==0.2.79 --upgrade --force-reinstall --no-cache-dir --extra-index-url https://abetlen.github.io/llama-cpp-python/whl/cu122
 
 
-pip install llama-cpp-python==0.2.79 numpy==1.26.4 pandas==2.1.4 --force-reinstall --no-cache-dir --extra-index-url https://abetlen.github.io/llama-cpp-python/whl/cu122
+pip install llama-cpp-python==0.2.68-cu122 numpy==1.26.4 pandas==2.1.4 --force-reinstall --no-cache-dir --extra-index-url https://abetlen.github.io/llama-cpp-python/whl/cu122
 
