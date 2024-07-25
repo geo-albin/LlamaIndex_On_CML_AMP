6c3b97f3cb87ffdd59293683716bbaaf6b1a356a
Albin George <ageorge@cloudera.com>
2024-07-25 13:42:18 -0400

    trying llamacpp latest

--- 0_session-install-dependencies/setup_gpu.sh
+++ 0_session-install-dependencies/setup_gpu.sh
@@ -2,13 +2,7 @@
 
 echo "installing GPU version of llama-cpp"
 
-# pip install llama-cpp-python==0.2.79 --extra-index-url https://abetlen.github.io/llama-cpp-python/whl/cu122
+pip install numpy==1.26.4 pandas==2.1.4 llama-cpp-python==0.2.82 --force-reinstall --no-cache-dir --extra-index-url https://abetlen.github.io/llama-cpp-python/whl/cu122
 
-# pip install llama-cpp-python==0.2.79 --upgrade --force-reinstall --no-cache-dir --extra-index-url https://abetlen.github.io/llama-cpp-python/whl/cu122
-
-
-# pip install llama-cpp-python==0.2.68 numpy==1.26.4 pandas==2.1.4 --force-reinstall --no-cache-dir --extra-index-url https://abetlen.github.io/llama-cpp-python/whl/cu122
-
-pip install numpy==1.26.4 pandas==2.1.4 llama-cpp-python==0.2.74 --force-reinstall --no-cache-dir --extra-index-url https://abetlen.github.io/llama-cpp-python/whl/cu122
-
+# CMAKE_ARGS="-DLLAMA_CUBLAS=on" FORCE_CMAKE=1 pip install numpy==1.26.4 pandas==2.1.4 llama-cpp-python==0.2.82 --force-reinstall --no-cache-di
 
